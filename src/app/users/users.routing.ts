7377cc3e07a99ae283bc92fddf3b2ef6ffcf19e4
OSorokin <olegsorokinpnz@gmail.com>
2017-03-10 16:15:59 +0300

    add-edit-component has been added.
    (cherry picked from commit 350a037)
    
    (cherry picked from commit b673b01)

--- src/app/users/users.routing.ts
+++ src/app/users/users.routing.ts
@@ -5,10 +5,12 @@ import { EditUserComponent } from './edit-user/edit-user.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
+import { AddEditUserComponent } from './add-edit-user/add-edit-user.component';
+
 const routes: Routes = [
   {path: 'users', component: UsersListComponent},
-  {path: 'users/add', component: AddUserComponent},
-  {path: 'users/edit/:id', component: EditUserComponent}
+  {path: 'users/add', component: AddEditUserComponent},
+  {path: 'users/edit/:id', component: AddEditUserComponent}
 ];
 
 @NgModule({
